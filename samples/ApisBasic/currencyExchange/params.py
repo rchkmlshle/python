f6e00f1e15611eda0854298e9490d225de875115
rchkmlshle <rchkmlshle@gmail.com>
2025-04-11 11:56:32 -0400

    exchange rates

---
+++ samples/ApisBasic/currencyExchange/params.py
@@ -0,0 +1,16 @@
+{
+"apis":{    
+    
+    "bucket":"exchangeratereview",
+    "exchangeratesapi":{
+        "url":"https://api.exchangeratesapi.io/latest",
+        "key":""
+    }
+},
+"gits":{
+        "apis": [
+"https://github.com/ptyadana/Web-Scraping-and-API-in-Python/tree/master/03.Working%20with%20APIs"
+
+]}
+
+}
