53351acec041b6b0400f932e396128bd77bc120f
ruhi c <rchkmlshle@gmail.com>
2025-04-16 15:20:21 -0400

    exchange proj

--- samples/ApisBasic/currencyExchange/exchangeRateApi.py
+++ samples/ApisBasic/currencyExchange/exchangeRateApi.py
@@ -13,32 +13,44 @@ class ProcessApi:
             config = json.load(f)
         self.url = config[item]["url"]
         self.api_key=config[item]["key"]
-        self.topics=config[item]["request_"]
-        self.bucket=config["bucket"]
+        self.bucket=config[item]["bucket"]
+        try:
+            self.filename=config[item]["filename"]
+        except:
+            self.filename=None
 
         self.payload = {}
         self.headers = {}
 
-    def GetReq(self, tag, filters):
+    def GetReq(self, filters:str=None):
         #TODO::: SET UP PAGES.
         try:
-            if (self.api_key):
-                url1=self.url + tag + "?/" + filters +"&" + "api_key=" +self.api_key
+            #self.url1=self.url  + "?access_key=" +self.api_key
+            if (filters):
+                url1=self.url + "?/" + filters +"&" + "access_key=" +self.api_key
             else:
-                url1=self.url + tag + "?/" + filters
-
-            response_data = requests.request("GET", url1, headers=self.headers, data=self.payload)
+                url1=self.url +  "?"  + "access_key=" +self.api_key
+            
+            response_data = requests.get(url1)
             response_df=pandas.DataFrame(json.loads(response_data.text))
+
+            #response review
+            print(response_data.status_code)
+
         except:
             print(logging.exception)
         return response_df
 
     def NormData(self, data_df, norm_criteria):
         normdata=pandas.json_normalize(data_df[norm_criteria])
+        print(normdata.columns)
         return normdata
     #TODO, MULTILEVEL NORM. NESTED STRUCTURES
 
-    def S3Load(self,normdata,filename):
+    def S3Load(self,normdata, filename=None):
+        if not (filename):
+            filename=self.filename
+
         csv_buffer = StringIO()
         normdata.to_csv(csv_buffer)
         s3_resource = boto3.resource('s3')
@@ -50,11 +62,26 @@ class ProcessApi:
             topic_data=self.GetReq(topic["tag"], topic["filters"])
             normdata=self.NormData(topic_data, norm_criteria)
             s3_load_status=self.S3Load(normdata, topic["file_name"])
-
-
+            
     def DeleteS3File(self):
-        for topic in self.topics:
-            s3 = boto3.resource('s3')
-            s3.Object(self.bucket, topic["file_name"]).delete()
+        s3 = boto3.resource('s3')
+        if not (self.filename):
+            for topic in self.topics:
 
- 
+                try:
+                    s3.Object(self.bucket, topic["file_name"]).delete()
+                except:
+                    print("chose different topic")
+        else:
+                s3.Object(self.bucket, self.filename).delete()
+
+def main():
+    
+    basic1=ProcessApi("exchange_basic") # simple exchange df
+    #Process Instance1
+    data=basic1.GetReq()
+    wr=basic1.S3Load(data)
+    del1=basic1.DeleteS3File()
+
+if __name__=='__main__':
+    main()
